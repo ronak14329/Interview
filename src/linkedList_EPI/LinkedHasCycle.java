96a6945ea0319473b912b9b4c351eae8626278f3
ronak14329 <ronak.goyal29@gmail.com>
2017-12-05 13:49:08 -0800

    hascycleLinkedList

---
+++ src/linkedList_EPI/LinkedHasCycle.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package linkedList_EPI;
+
+/**
+ * @author Ronak Goyal
+ *Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+
+
+ */
+public class LinkedHasCycle {
+
+	/**
+	 * @param args
+	 */
+	
+	public static boolean hasCycle(ListNode head){
+		ListNode slow = head;
+		ListNode fast = head;
+		while(fast.next != null && fast.next.next != null){
+			slow = slow.next;
+			fast = fast.next.next;
+			if(slow == fast)return true;
+		}
+		return false;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+        head.next.next.next.next.next.next = head.next.next.next;
+        
+        System.out.println(hasCycle(head));
+	}
+
+}

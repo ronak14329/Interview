4d278301e1dbe0b8d0357d3710b495e6689020aa
ronak14329 <ronak.goyal29@gmail.com>
2017-12-05 22:37:10 -0800

    DetectCycleInLinkedList

---
+++ src/linkedList_EPI/LinkedListCycle.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package linkedList_EPI;
+
+/**
+ * @author Ronak Goyal
+ *Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+Note: Do not modify the linked list.
+
+Follow up:
+Can you solve it without using extra space?
+
+
+ */
+public class LinkedListCycle {
+
+	/**
+	 * @param args
+	 */
+	public static ListNode detectCycle(ListNode head){
+		ListNode slow = head;
+		ListNode fast = head;
+		while(fast != null && fast.next!=null){
+			slow = slow.next;
+			fast = fast.next.next;
+			if(slow==fast){
+				while(head!=slow){
+					head = head.next;
+					slow = slow.next;
+				}return slow;
+			}
+		}return null;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ListNode head = new ListNode(4);
+        head.next = new ListNode(7);
+        head.next.next = new ListNode(9);
+        head.next.next.next = new ListNode(5);
+        head.next.next.next.next = new ListNode(1);
+        head.next.next.next.next.next = new ListNode(10);
+        head.next.next.next.next.next.next = head.next.next;
+        
+        ListNode res = detectCycle(head);
+        System.out.println(res.val);
+//        while(res!= null){
+//        	System.out.print(res.val +"->");
+//        	res=  res.next;
+//        }
+	}
+
+}
